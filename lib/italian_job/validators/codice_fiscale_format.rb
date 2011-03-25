23b22a865bff1d50134552b72fedf462c777e9ec
lucapette <lucapette@gmail.com>
2011-03-25 18:07:24 +0100

    new example

--- lib/italian_job/validators/codice_fiscale_format.rb
+++ lib/italian_job/validators/codice_fiscale_format.rb
@@ -1,11 +1,20 @@
 module ActiveModel
     module Validations
         class CodiceFiscaleFormatValidator < ActiveModel::EachValidator
+            REGEX=Regexp.compile("^[A-Za-z]{6}[0-9]{2}[A-Za-z][0-9]{2}[A-Za-z][0-9]{3}[A-Za-z]$")
 
             def validate_each(object, attribute, value)
-                object.errors[attribute] << I18n.translate("activerecord.errors.codice_fiscale.empty") if value.blank?
+                if value.blank?
+                    object.errors[attribute] << I18n.translate("activerecord.errors.codice_fiscale.empty")
+                    return
+                end
+                if value != value.upcase
+                end
+                unless value.match(REGEX)
+                    bject.errors[attribute] << I18n.translate("activerecord.errors.codice_fiscale.invalid_format") 
+                    return
+                end
             end
-
         end
     end
 end
--- spec/italian_job/validators/codice_fiscale_spec.rb
+++ spec/italian_job/validators/codice_fiscale_spec.rb
@@ -27,6 +27,11 @@ describe "codice_fiscale_validator" do
             @user.codice_fiscale = code
             @user.should have(1).error_on(:codice_fiscale)
         end
+
+        it "should have an error on #{code}" do
+            @user.should have(1).errors_on(:codice_fiscale)
+            @user.errors[:codice_fiscale].should == [I18n.translate("activerecord.errors.codice_fiscale.invalid_format")]
+        end
     }
 
 end
