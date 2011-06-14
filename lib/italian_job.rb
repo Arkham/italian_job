facb0f175abfd4be57c186fd9245022ec918c9e0
Stefano Verna <stefano.verna@gmail.com>
2011-06-14 10:53:02 +0200

    Added helpers

--- lib/italian_job.rb
+++ lib/italian_job.rb
@@ -1,11 +1,12 @@
 require "italian_job/validators"
 
 if Rails::VERSION::MAJOR >= 3
-  # include InstanceMethods to expose the ExistenceValidator class to ActiveModel::Validations
-  ActiveModel::Validations.__send__(:include, ItalianJob::Validators::InstanceMethods)
-
-  # extend the ClassMethods to expose the validates_presence_of method as a class level method of ActiveModel::Validations
-  ActiveModel::Validations.__send__(:extend, ItalianJob::Validators::ClassMethods)
+  #puts "CIAO"
+  ## include InstanceMethods to expose the ExistenceValidator class to ActiveModel::Validations
+  #ActiveModel::Validations.__send__(:include, ItalianJob::Validators::InstanceMethods)
+  #
+  ## extend the ClassMethods to expose the validates_presence_of method as a class level method of ActiveModel::Validations
+  #ActiveModel::Validations.__send__(:extend, ItalianJob::Validators::ClassMethods)
 else
   require "rails2/validator"
   ActiveRecord::Base.__send__(:extend, ItalianJob::Validators)
